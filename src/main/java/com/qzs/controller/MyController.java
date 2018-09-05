e7c7c2a92b703ab240d742b70a3310d8a78ec212
qiuzhenshuo <2635430271@qq.com>
2018-09-05 20:33:17 +0800

    dfg

--- src/main/java/com/qzs/controller/MyController.java
+++ src/main/java/com/qzs/controller/MyController.java
@@ -24,7 +24,7 @@ public class MyController {
 		request.setAttribute("list", list);
 		System.out.println("--------------------sdfafsadfasdf================");
 		System.out.println("23423423423");
-		System.out.println("////////////////////////////////////");
+		System.out.println("dfgsdfgsdf");
 		return "list";
 	}
 	
